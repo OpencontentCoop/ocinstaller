f5c1cc8a8c4676fbedbbde7728d3b07f08656394
Luca Realdi <lr@opencontent.it>
2020-10-01 10:22:16 +0200

    Now inside a resource can import another resource

--- src/Opencontent/Installer/IOTools.php
+++ src/Opencontent/Installer/IOTools.php
@@ -15,6 +15,8 @@ class IOTools
      */
     protected $installerVars;
 
+    private static $avoidImportRecursion = [];
+
     public function __construct($dataDir, $installerVars)
     {
         $this->dataDir = $dataDir;
@@ -49,14 +51,42 @@ class IOTools
         return false;
     }
 
-    public function getJsonContents($source)
+    public function getJsonContents($source, $parentSource = null)
     {
         $filePath = $this->getFile($source);
+
+        $avoidRecursionKey = $parentSource ? $parentSource : $source;
+        if (!isset(self::$avoidImportRecursion[$avoidRecursionKey])){
+            self::$avoidImportRecursion[$avoidRecursionKey] = [];
+        }
 
         if (file_exists($filePath)) {
             $data = file_get_contents($filePath);
             $data = $this->installerVars->filter($data);
             $json = Yaml::parse($data);
+
+            if (isset($json['imports'])){
+                if (in_array($source, self::$avoidImportRecursion[$avoidRecursionKey])){
+                    throw new \Exception("Found import recursion {$source} in {$avoidRecursionKey}");
+                }
+                self::$avoidImportRecursion[$avoidRecursionKey] = $source;
+                foreach ($json['imports'] as $import){
+                    if (isset($import['resource'])){
+                        if (in_array($import['resource'], self::$avoidImportRecursion[$avoidRecursionKey])){
+                            throw new \Exception("Found import recursion {$import['resource']} in {$avoidRecursionKey}");
+                        }
+                        self::$avoidImportRecursion[$avoidRecursionKey] = $import['resource'];
+                        $importData = $this->getJsonContents($import['resource'], $avoidRecursionKey);
+                        if (!$importData){
+                            throw new \Exception("Fail importing resource {$import['resource']}");
+                        }
+                        $json = array_merge_recursive($json, $importData);
+                    }
+                }
+                unset($json['imports']);
+                self::$avoidImportRecursion[$avoidRecursionKey] = [];
+            }
+
             $this->installerVars->validate($json, $source);
 
             return $json;
