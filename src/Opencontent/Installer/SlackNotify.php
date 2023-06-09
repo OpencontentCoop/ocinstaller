54f5b25c6f8fcc61a118167c25cf36a7e3c2d4d2
Luca Realdi <luca.realdi@opencontent.it>
2023-06-09 23:59:44 +0200

    Fix slack integration

--- src/Opencontent/Installer/SlackNotify.php
+++ src/Opencontent/Installer/SlackNotify.php
@@ -4,22 +4,17 @@ namespace Opencontent\Installer;
 
 class SlackNotify
 {
-    public static function notify($token, $channel, $message)
+    public static function notify($slackEndpoint, $message)
     {
-        if (!empty($token) && !empty($channel)) {
-            $ch = curl_init("https://slack.com/api/chat.postMessage");
-            $data = http_build_query([
-                "token" => $token,
-                "channel" => $channel,
-                "text" => $message,
-                "username" => "PusherBot",
-            ]);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_exec($ch);
-            curl_close($ch);
-        }
+        $ch = curl_init($endpoint);
+        $data = json_encode([
+            "text" => $message,
+        ]);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_exec($ch);
+        curl_close($ch);
     }
 }
